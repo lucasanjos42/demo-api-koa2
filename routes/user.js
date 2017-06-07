b61f96e06727ed22a66e37b96257498e15221235
Lucas Anjos <lucas.cavalcante.anjos@gmail.com>
2017-06-07 01:00:25 -0300

    connecting to mongolab and post/put routes

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,63 @@ const User = require('../models/user');
 
 router.get('/user', async(ctx) => {
 
+    const users = await User.find({});
+
     ctx.body = {
-        response: 'node <3'
-    }
+        users
+    };
 
 });
 
 router.post('/user', async ctx => {
 
-    ctx.body = ctx.request.body;
+    try {
+
+        const user = new User(ctx.request.body);
+
+        await user.save();
+
+        ctx.body = {
+            user
+        };
+
+    } catch (error) {
+
+        ctx.body = {
+            error
+        };
+
+        ctx.status = 500;
+
+    }
+
+});
+
+router.put('/user/:id', async ctx => {
+
+    try {
+
+        const id = ctx.params.id;
+
+        const param = {
+            '_id': id
+        };
+
+        const user = await User.update(param, ctx.request.body);
+
+        ctx.body = {
+            user
+        };
+
+    } catch (error) {
+
+        ctx.body = {
+            error
+        };
+
+        ctx.status = 500;
+
+    }
 
 });
 
